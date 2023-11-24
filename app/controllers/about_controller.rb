cb9bc6dc4c3b4c75d1eb7f165ec69f5323643e8a
harshita3199 <harshita.gupta@scilifelab.se>
2023-11-24 13:31:57 +0100

    Added comment for demo

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,6 @@
 # The controller for actions related to the about pages
 class AboutController < ApplicationController
-
+ #added comment
   skip_before_action :authenticate_user!, :authenticate_user_from_token!
   
   def tess
